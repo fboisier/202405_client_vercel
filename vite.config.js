6a243cecabe3ab0afad6096345cc80db6027099e
Francisco Boisier <fboisier@codingdojo.la>
2024-08-06 21:57:05 -0400

    se cambia a variables de ambiente

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_ENDPOINT,
         changeOrigin: true,
         secure: false,
       },
